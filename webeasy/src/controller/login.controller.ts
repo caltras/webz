9ca530c7b8dd659b65d1748696d4385f837113a6
Claudio Traspadini Oliveira <caltras@gmail.com>
2018-03-27 13:46:33 +0100

    working in login feature

--- webeasy/src/controller/login.controller.ts
+++ webeasy/src/controller/login.controller.ts
@@ -1,6 +1,7 @@
 import {BaseController} from './base.controller';
-import {Controller, Inject, Get} from '../decorators'
+import {Controller, Inject, Get, Post} from '../decorators'
 import { ServerRequest, ServerResponse } from 'http';
+import { FormParameter } from '.';
 
 @Controller("/login")
 export class LoginController extends BaseController{
@@ -8,5 +9,9 @@ export class LoginController extends BaseController{
     @Get({ url: "/"})
     public login(request:ServerRequest,response:ServerResponse){
         return this.render("view/login.html");
+    }
+    @Post({url:"/"})
+    public postLogin(request:ServerRequest,response:ServerResponse,body:FormParameter){
+        this.redirect("/",response);
     }
 }
