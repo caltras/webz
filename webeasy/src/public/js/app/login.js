b39a31655e72bb85cea82f19d8896e7ecece0d0c
Claudio Traspadini Oliveira <caltras@gmail.com>
2018-04-03 22:41:42 +0100

    signup ui

---
+++ webeasy/src/public/js/app/login.js
@@ -0,0 +1,8 @@
+(()=>{
+    document.getElementsByClassName('btn-signup')[0].onclick= (event)=>{
+        document.getElementsByClassName('flip-container')[0].classList.toggle('hover');
+    };
+    document.getElementsByClassName('btn-cancel')[0].onclick= (event)=>{
+        document.getElementsByClassName('flip-container')[0].classList.toggle('hover');
+    };
+})();
